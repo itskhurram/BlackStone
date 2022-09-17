04063305616fcfad1e92841a618205e37889cce6
itskhurram <itskhurrams@outlook.com>
2022-09-18 00:51:35 +0500

    CategoryRepository

---
+++ src/BlackStone.Persistance/Repositories/CategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using BlackStone.Core.Entities;
+using BlackStone.Core.Interfaces;
+
+namespace BlackStone.Persistance.Repositories {
+	public class CategoryRepository : ICategoryRepository {
+		public Task<IList<Category>> GetAllCategories(bool? isActive = null) {
+			throw new NotImplementedException();
+		}
+	}
+}
