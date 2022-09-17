a81981679c2101a23cf96d084b21002f28cdac9f
itskhurram <itskhurrams@outlook.com>
2022-09-18 00:31:41 +0500

    Models Updated with Memory Cache Interface

---
+++ src/BlackStone.Core/Models/JWToken.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlackStone.Core.Models {
+	public class JWToken {
+		[Required]
+		public string? Token { get; set; }
+		[Required]
+		public string? RefreshToken { get; set; }
+		public DateTime TokenExpirationTimeInMinutes { get; set; }
+		public DateTime RefreshTokenExpirationTimeInMinutes { get; set; }
+	}
+}
---
+++ src/BlackStone.Core/Models/MemoryCacheKeys.cs
@@ -0,0 +1,6 @@
+﻿namespace BlackStone.Core.Models {
+	public static class MemoryCacheKeys {
+		public static string? Entry => "_Entry";
+		public static string? Category => "_Category";
+	}
+}
