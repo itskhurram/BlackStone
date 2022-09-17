24af481fa3ab75039b0c4d4d1f36529ac60903e9
itskhurram <itskhurrams@outlook.com>
2022-09-18 00:50:21 +0500

    AccountType Added

---
+++ src/BlackStone.Persistance/Repositories/AccountTypeRepository.cs
@@ -0,0 +1,10 @@
+﻿using BlackStone.Core.Entities;
+using BlackStone.Core.Interfaces;
+
+namespace BlackStone.Persistance.Repositories {
+	public class AccountTypeRepository : IAccountTypeRepository {
+		public Task<IList<AccountType>> GetAllAccountTypes(bool? isActive = null) {
+			throw new NotImplementedException();
+		}
+	}
+}
