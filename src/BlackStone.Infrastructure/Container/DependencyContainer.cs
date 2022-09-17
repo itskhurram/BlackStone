253612164314cc20fb8f1c6ba6732163e26efc6e
itskhurram <itskhurrams@outlook.com>
2022-09-18 00:41:04 +0500

    IOC containers

---
+++ src/BlackStone.Infrastructure/Container/DependencyContainer.cs
@@ -0,0 +1,34 @@
+﻿using BlackStone.Core.Interfaces;
+using BlackStone.Infrastructure.MemoryCache;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlackStone.Infrastructure.Container {
+	public class DependencyContainer {
+		public static void RegisterServices(IServiceCollection services) {
+			ServicesRegistration(services);
+			RepositoryRegistration(services);
+			ApploggerRegistration(services);
+		}
+		private static void ServicesRegistration(IServiceCollection services) {
+			//services.AddTransient<IAccountTypeService, AccountTypeService>();
+			//services.AddTransient<ICategoryService, CategoryService>();
+			//services.AddTransient<IUserService, UserService>();
+			//services.AddTransient<IRefreshTokenService, RefreshTokenService>();
+			//services.AddTransient<ILoginLogService, LoginLogService>();
+		}
+		private static void RepositoryRegistration(IServiceCollection services) {
+			//services.AddTransient<IBaseRepository, BaseRepository>();
+			//services.AddTransient<IAccountTypeRepository, AccountTypeRepository>();
+			//services.AddTransient<ICategoryRepository, CategoryRepository>();
+			//services.AddTransient<IUserRepository, UserRepository>();
+			//services.AddTransient<IRefreshTokenRepository, RefreshTokenRepository>();
+			//services.AddTransient<ILoginLogRepository, LoginLogRepository>();
+			services.AddScoped<IMemoryCacheProvider, MemoryCacheProvider>();
+		}
+
+		private static void ApploggerRegistration(IServiceCollection services) {
+			//services.AddSingleton<IApplogger, Applogger>();
+		}
+	}
+}
